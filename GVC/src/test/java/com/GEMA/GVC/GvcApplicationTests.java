5c719a295884081a6aaaefbdb5e02045758cb164
Erick Osuna Garcia <127696393+Erickzzxc@users.noreply.github.com>
2024-10-15 22:08:46 -0700

    Initial commit

---
+++ GVC/src/test/java/com/GEMA/GVC/GvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GEMA.GVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
